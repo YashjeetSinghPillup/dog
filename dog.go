aefafe0eeb3b159323f19971872b519346ff7ef9
YashjeetSinghPillup <yashjeet.singh@pillup.com>
2024-01-24 07:52:51 +0530

    Update dog.go

--- dog.go
+++ dog.go
@@ -3,5 +3,5 @@ package dog
 import "strings"
 
 func WhenGrownUp(s string) string {
-	return "Version 1.0.0: When puppy grows up it says: " + strings.ToUpper(s)
+	return "Version 1.2.0: When puppy grows up it says: " + strings.ToUpper(s)
 }
